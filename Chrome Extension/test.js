bf0b8a9ea69269c6e19455cfed49884c47d381af
Andreas Jevnaker <andreasjevn@gmail.com>
2020-07-25 18:58:38 +0200

    Finished generating ean and outputting to webpage
    Continuing to figure out how to grab value of EAN from product info section of the page and provde it for the generator script.

--- Chrome Extension/test.js
+++ Chrome Extension/test.js
@@ -1,8 +1,12 @@
+setTimeout(function main(){
+  
 var submitBtn = document.getElementById("submit");
 var barcodeOut = document.getElementById("barcode1");
 var grabDom = document.getElementById("misc-info");
 var newDiv = document.createElement("div");
-var grabEan = document.getElementsByClassName("any-3-4 S-1-2")
+
+//var grabEan = document.querySelector("any-3-4 S-1-2")[0];
+var grabEan = document.querySelectorAll("any-3-4 S-1-2")[0];
 
 newDiv.innerHTML = `
 
@@ -11,14 +15,24 @@ newDiv.innerHTML = `
 
 `;
 
+
 grabDom.appendChild(newDiv);
 
-JsBarcode("#barcode1", "3030050149845", {
+alert(grabEan.nodeName);
+
+var eanFill = 303005014999;
+
+JsBarcode("#barcode1", eanFill, {
     format:"EAN13",
 			  displayValue:true,
 			  fontSize:24,
 			  lineColor: "black"
 });
+
+}, 3000);
+
+
+
 
 
 
